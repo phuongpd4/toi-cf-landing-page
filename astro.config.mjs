48b3001c61e4a28eb40c00480d8d8d444fc2012a
Phuong Pham <phuongphamgh@gmail.com>
2026-03-05 21:47:34 +0700

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,9 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://phuongpd4.github.io', /* Cần thay bằng username github của bạn */
+  base: '/toi-cf-landing-page', /* Tên chính xác của Repository trên GitHub */
+
   integrations: [react()],
 
   vite: {
